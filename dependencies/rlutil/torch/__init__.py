378fb4447c495d747cfaac6164dc3432d7ea6521
Soumith Udatha <sudatha@matrix-0-24.eth>
2022-01-21 17:53:45 -0500

    cluster changes

--- dependencies/rlutil/torch/__init__.py
+++ dependencies/rlutil/torch/__init__.py
@@ -2,6 +2,7 @@ import rlutil.torch.nn
 from rlutil.torch.pytorch_util import set_gpu, default_device, to_numpy
 from types import ModuleType
 import torch.optim as optim
+import pdb
 
 def _replace_funcs(global_dict):
     import torch as th
@@ -18,14 +19,22 @@ def _replace_funcs(global_dict):
             return '<device-wrapped function %s>' % self.fn.__name__
 
     for _key in dir(th):
-        _value = getattr(th, _key) 
-        # hacky way of determining if device is an argument
-        # (cannot use inspect because torch functions are builtins)
-        if _value.__doc__ and 'device (:class:`torch.device`, optional):' in _value.__doc__:
-            global_dict[_key] = DeviceWrapped(_value)
-        elif isinstance(_value, ModuleType):
-            pass
-        else:
+        _value = getattr(th, _key)
+       # print(_key)
+        if _key in ['ScriptClass']:
             global_dict[_key] = _value
+            continue
+        else: 
+        # pdb.set_trace()       
+        # hacky way of determining if device is an argument
+        # (cannot use inspect because torch functions are builtins)   
+           # print(_key)
+            if isinstance(_value, ModuleType):
+                pass
+            elif _value.__doc__ and 'device (:class:`torch.device`, optional):' in _value.__doc__:
+                global_dict[_key] = DeviceWrapped(_value)
+               # print('In',_key)
+            else:
+                global_dict[_key] = _value
 
 _replace_funcs(globals())
